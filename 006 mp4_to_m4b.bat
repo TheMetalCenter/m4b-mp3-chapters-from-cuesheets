b50674c35c81cc35aa7a877dcc6010b1f969e706
siteuser42 <49706654+siteuser42@users.noreply.github.com>
2021-08-26 12:20:52 -0500

    Add files via upload

--- 006 mp4_to_m4b.bat
+++ 006 mp4_to_m4b.bat
@@ -1,6 +1,9 @@
 @echo off
-ffprobe output.mp4 -show_entries format_tags=title -of compact=p=0:nk=1 -v 0 > title.txt
+::Copies the album field to a temp text file and sets variable
+ffprobe output.mp4 -show_entries format_tags=album -of compact=p=0:nk=1 -v 0 > title.txt
 set /p name=< title.txt
+
+::Converts all mp4 to m4b with nero/quicktime chapters (will fail on input.mp4 since no chapters)
 FOR %%i IN (*.mp4) DO (
  echo converting %%i
  mp4chaps.exe -QuickTime -convert "%%i"
@@ -9,6 +12,8 @@ FOR %%i IN (*.mp4) DO (
 )
 echo ---------------------
 echo  CONVERTING COMPLETE, moving file to folder
+
+::Renames output to album and moves to folder of same name
 rename output.m4b "%name%.m4b"
 md "%name%"
 MOVE "%name%.m4b" "%name%"
