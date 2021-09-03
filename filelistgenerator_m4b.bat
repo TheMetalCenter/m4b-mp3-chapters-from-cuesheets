6719aed6bcce74881d0d3d6fc394932cb961da27
siteuser42 <49706654+siteuser42@users.noreply.github.com>
2021-09-02 21:41:08 -0500

    Add files via upload

--- filelistgenerator_m4b.bat
+++ filelistgenerator_m4b.bat
@@ -1,4 +1,17 @@
 @echo off
+    setlocal
+    for %%a in (*!*.m4b) do call :remove "%%~a"
+:remove
+    set "FROM=%~1" >nul 2>&1
+    set "TO=%FROM:!=%" >nul 2>&1
+    ren "%FROM%" "%TO%" >nul 2>&1
+@echo off
+setlocal enabledelayedexpansion
+for /f "tokens=*" %%a in ('dir /b *.m4b') do (
+  set file=%%a >nul 2>&1
+  ren "!file!" "!file:&=and!" >nul 2>&1
+)
+@echo off
 setlocal disabledelayedexpansion
 for /f "tokens=*" %%f in ('dir /b *.m4b') do (
   set _a=%%~nf
