708f593fca3ebebf599f661d6346b138283e9697
siteuser42 <49706654+siteuser42@users.noreply.github.com>
2021-09-04 21:48:47 -0500

    Add files via upload

--- mp3_merge-files.bat
+++ mp3_merge-files.bat
@@ -1,18 +1,18 @@
 :fix_input
-::This renames inputs to remove all exclamation marks (!), apostrophes ('), and ampersands (&)
 @echo off
+
+::Remove "!" characters:
     setlocal
-    for %%a in (*!*.mp3) do call :remove1 "%%~a"
-:remove1
-    set "FROM=%~1" >nul 2>&1
-    set "TO=%FROM:!=%" >nul 2>&1
-    ren "%FROM%" "%TO%" >nul 2>&1
-    setlocal
-    for %%b in (*'*.mp3) do call :remove2 "%%~b"
-:remove2
-    set "FROM=%~1" >nul 2>&1
-    set "TO=%FROM:'=%" >nul 2>&1
-    ren "%FROM%" "%TO%" >nul 2>&1
+    for %%a in (*!*.mp3) do call :remove "%%~a"
+     
+::Remove "'" characters:
+setlocal EnableDelayedExpansion
+for %%a in ("*'*.mp3") do (
+   set "fileName=%%~NXa"
+   ren "%%a" "!filename:'=!"
+)
+
+::Remove "&" characters:
 setlocal enabledelayedexpansion
 for /f "tokens=*" %%c in ('dir /b *.mp3') do (
   set file=%%c >nul 2>&1
@@ -23,7 +23,6 @@ for /f "tokens=*" %%c in ('dir /b *.mp3') do (
 call :generate_filelist > filelist.txt
 
 :: This copies metadata from the first input file, using temporary files to get formatting correct
-:copy_meta
 SetLocal EnableDelayedExpansion
 set inputfix=filelist.txt
 set outputfix=inputmeta1.txt
@@ -70,3 +69,9 @@ for /f "tokens=*" %%f in ('dir /b *.mp3') do (
   )
 endlocal
 EXIT /B 0
+
+:remove
+    set "FROM=%~1"
+    set "TO=%FROM:!=%"
+    ren "%FROM%" "%TO%"
+    
