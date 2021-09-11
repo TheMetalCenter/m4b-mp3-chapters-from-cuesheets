941f61e7a86b0a80d033ed68a673564aaa776b5c
siteuser42 <49706654+siteuser42@users.noreply.github.com>
2021-09-11 00:18:50 -0500

    Add files via upload

---
+++ m4b_list2cue.bat
@@ -0,0 +1,33 @@
+@echo off
+:check1
+if exist "input.mp4" (
+goto :check2
+) else goto :stop1
+
+:check2
+if exist "list.txt" (
+goto :check3
+) else goto :stop2
+
+:check3
+if exist "list2cue.py" (
+goto :list2cue
+) else goto :stop3
+
+:list2cue
+python list2cue.py list.txt --audio-file="input.mp4" --output-file="cuesheet.cue"
+
+:stop1
+echo MISSING input.mp4
+pause
+exit
+
+:stop2
+echo MISSING list.txt
+pause
+exit
+
+:stop3
+echo MISSING list2cue.py
+pause
+exit
---
+++ mp3_list2cue.bat
@@ -0,0 +1,34 @@
+@echo off
+:check1
+if exist "input.mp3" (
+goto :check2
+) else goto :stop1
+
+:check2
+if exist "list.txt" (
+goto :check3
+) else goto :stop2
+
+:check3
+if exist "list2cue.py" (
+goto :list2cue
+) else goto :stop3
+
+:list2cue
+py list2cue.py list.txt --audio-file="input.mp3" --output-file="cuesheet.cue"
+exit
+
+:stop1
+echo MISSING input.mp3
+pause
+exit
+
+:stop2
+echo MISSING list.txt
+pause
+exit
+
+:stop3
+echo MISSING list2cue.py
+pause
+exit
